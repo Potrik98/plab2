bc5a377a3935388b4cac8b3cba77821e8d061f0e
potrik98 <potrik98@gmail.com>
2018-09-24 18:13:09 +0200

    proj3 setup

---
+++ proj3/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='crypto',
+      version='0.1',
+      description='Crypto',
+      url='http://github.com/potrik98/plab2',
+      author='Erling Rorvik',
+      author_email='potrik98@gmail.com',
+      license='MIT',
+      packages=['crypto'],
+      install_requires=[],
+      test_suite='nose.collector',
+      tests_require=['nose'],
+      zip_safe=False)
