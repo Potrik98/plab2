0fd44ab8429809daf5dc8fdfc9f6cfc4ad9f9081
potrik98 <potrik98@gmail.com>
2018-10-12 00:30:18 +0200

    Add proj4 skeleton

---
+++ proj4/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='calculator',
+      version='0.1',
+      description='calculator',
+      url='http://github.com/potrik98/plab2',
+      author='Erling Rorvik',
+      author_email='potrik98@gmail.com',
+      license='MIT',
+      packages=['data', 'calculator'],
+      install_requires=[],
+      test_suite='nose.collector',
+      tests_require=['nose'],
+      zip_safe=False)
