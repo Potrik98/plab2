24a3d35d3ae584079b99fe9fa67b3c0341807876
potrik98 <potrik98@gmail.com>
2018-10-20 20:32:27 +0200

    Add light bindings

--- proj5/LedController.py
+++ proj5/LedController.py
@@ -1,5 +1,6 @@
 import time
 import RPi.GPIO as GPIO
+from threading import Thread
 
 lightpins = [18, 23, 24]
 lights = [[1, -1, 0], [0, 1, -1], [-1, 1, 0],
@@ -10,8 +11,9 @@ class LedController:
     def __init__(self):
         GPIO.setmode(GPIO.BCM)
         GPIO.setwarnings(False)
+        self._active_led = None
 
-    def led_on(self, which):
+    def _led_on(self, which):
         pattern = lights[which]
         for n in range(3):
             if pattern[n] == -1:
@@ -21,23 +23,49 @@ class LedController:
                 GPIO.setup(lightpins[n], GPIO.OUT)
                 GPIO.output(lightpins[n], GPIO.HIGH)
 
-    def led_off(self):
+    def _led_off(self):
         for n in range(3):
             GPIO.setup(lightpins[n], GPIO.IN)
 
-    def hold_led(self, which, duration):
-        self.led_on(which)
+    def _hold_led(self, which, duration):
+        self._led_on(which)
         time.sleep(duration)
-        self.led_off()
+        self._led_off()
 
-    def twinkle_all_leds(self, duration):
-        for n in range(6):
-            self.hold_led(n, duration)
+    def _resume_state(self):
+        if self._active_led is not None:
+            self._led_on(self._active_led)
+        else:
+            self._led_off()
+
+    def twinkle_all_leds(self, times, duration):
+        for _ in range(times):
+            for n in range(6):
+                self._hold_led(n, duration / 6)
+        self._resume_state()
 
     def power_up_leds(self, duration):
-        for n in range(6):
-            self.hold_led((n * 2) % 5, duration)
+        for _ in range(3):
+            for n in range(6):
+                self._hold_led((n * 2) % 5, duration / 18)
+        self._resume_state()
 
     def power_down_leds(self, duration):
-        for n in range(6):
-            self.light_led(((5 - n) * 2) % 5, duration)
+        for _ in range(2):
+            for n in range(6):
+                self.light_led(((5 - n) * 2) % 5, duration / 12)
+        self._resume_state()
+        self._active_led = None
+
+    def set_led_duration(self, led_id, duration):
+        if (led_id >= len(lights)):
+            return
+        self._active_led = led_id
+        self._led_on(led_id)
+        t1 = Thread(target=lambda: self._turn_off_led_after(duration))
+        t1.start()
+
+    def _turn_off_led_after(self, led_id, duration):
+        time.sleep(duration)
+        self._led_off(led_id)
+        self._active_led = None
--- proj5/fsm/FSMController.py
+++ proj5/fsm/FSMController.py
@@ -1,9 +1,14 @@
+from LedController import LedController
 
 
 class FSMController:
     def __init__(self):
         self._correct_code = "666"
         self._state = None
+        self._led_controller = LedController()
+
+    def set_state(self, state) -> None:
+        self._state = state
 
     def get_correct_code(self) -> str:
         return self._correct_code
@@ -15,16 +20,16 @@ class FSMController:
         self._correct_code = code
 
     def set_led(self, led_id: int, duration: int) -> None:
-        pass  # TODO: use led controller
+        self._led_controller.set_led_duration(led_id, duration)
 
     def show_startup_lights(self) -> None:
-        pass  # TODO
+        self._led_controller.power_up_leds(1.0)
 
     def show_shutdown_lights(self) -> None:
-        pass  # TODO
+        self._led_controller.power_down_leds(1.0)
 
     def show_passcode_accepted_lights(self) -> None:
-        pass  # TODO
+        self._led_controller.twinkle_all_leds(3, 0.4)
 
     def show_error_lights(self) -> None:
         pass  # TODO
