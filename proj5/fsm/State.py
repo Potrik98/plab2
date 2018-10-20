d9ff2d9173453a037bd508ae572e3161283c49f2
potrik98 <potrik98@gmail.com>
2018-10-20 21:10:26 +0200

    Add logging

--- proj5/fsm/State.py
+++ proj5/fsm/State.py
@@ -48,10 +48,13 @@ class LoggedInState(State):
     def process_input(self, input: str) -> State:
         if is_int(input):
             self._action_identifier += input
+            print("action: %s" % self._action_identifier)
             return self
         elif input == '#':
+            print("executing action %s" % self._action_identifier)
             # execute the action with the right identifier
             if self._action_identifier not in self._actions:
+                print("invalid action!")
                 # invalid action, abort
                 self._fsm.show_error_lights()
                 return LoggedInState(self._fsm)
@@ -59,6 +62,7 @@ class LoggedInState(State):
                 # Go to the action state
                 return self._actions[self._action_identifier](self._fsm)
         else:
+            print("li: aborting execution")
             # abort
             return LoggedInState(self._fsm)
 
@@ -92,18 +96,20 @@ class InputCodeState(State):
         self._next_state = LoggedInState(fsm)
 
     def process_input(self, input: str) -> State:
-        print("ci: %s, so far: %s" % (input, self._code_so_far))
         if not is_int(input):
             return self  # Ignore any invalid input
         self._code_so_far += input
         self._inputs_so_far += 1
+        print("code: %s" % self._code_so_far)
         if self._inputs_so_far == self._code_length:
             # A code of the right length has been inputted
             if self._code_so_far == self._correct_code:
                 # Code is correct
+                print("success")
                 self._fsm.show_passcode_accepted_lights()
                 return self._next_state
             else:
+                print("invalid passcode!")
                 # Code is incorrect, return to the initial state
                 self._fsm.show_error_lights()
                 return RecieveInputState(self._fsm)
@@ -124,6 +130,7 @@ class InputCodeState(State):
 class ConfirmCodeState(InputCodeState):
     def __init__(self, fsm: FSMController, correct_code: str):
         super().__init__(fsm, correct_code)
+        print("confirm new passcode")
         self._next_state = SetCodeState(fsm, correct_code)
 
 
@@ -136,16 +143,21 @@ class ChangeCodeState(State):
         super().__init__(fsm)
         self._code_so_far = ""
         self._parent = ChangeCodeState
+        print("change passcode")
 
     def process_input(self, input: str) -> State:
         if is_int(input):
             # Add any number of inputted numbers to the code
             self._code_so_far += input
+            print("new code: %s" % self._code_so_far)
             return self
-        else:
-            # Any non numeric input terminates the code input
+        elif input == '#':
             # Enter the confirm code state.
             return ConfirmCodeState(self._fsm, self._code_so_far)
+        else:
+            # Abort
+            print("aborting")
+            return LoggedInState(self._fsm)
 
 
 #
@@ -155,6 +167,7 @@ class ChangeCodeState(State):
 class SetCodeState(State):
     def __init__(self, fsm: FSMController, new_code: str):
         super().__init__(fsm)
+        print("updating new code...")
         fsm.set_correct_code(new_code)
 
     def process_input(self, input: str) -> State:
@@ -170,16 +183,22 @@ class GetLedIdState(State):
     def __init__(self, fsm: FSMController):
         super().__init__(fsm)
         self._led_id = ""
+        print("enter led id")
 
     def process_input(self, input: str) -> State:
         if is_int(input):
             # Append the input to the led
             self._led_id += input
+            print("led id: %s" % self._led_id)
             return self
-        else:
-            # Duration input complete
+        elif input == '#' and self._led_id != "":
+            # Led id input complete
             # Proceed to the get duration state
             return GetLedDurationState(self._fsm, int(self._led_id))
+        else:
+            # Abort
+            print("aborting")
+            return LoggedInState(self._fsm)
 
 
 #
@@ -192,13 +211,19 @@ class GetLedDurationState(State):
         super().__init__(fsm)
         self._led_id = led_id
         self._duration = ""
+        print("enter led duration")
 
     def process_input(self, input: str) -> State:
         if is_int(input):
             # Append the input to the duration
             self._duration += input
+            print("duration: ")
             return self
-        else:
+        elif input == '#':
             # Duration input complete
+            if self._duration == "":
+                self._duration = "0"
+            print("setting led %d to duration %s" % (self._led_id,
+                                                     self._duration))
             self._fsm.set_led(self._led_id, int(self._duration))
             return LoggedInState(self._fsm)  # Return to the logged in state
