8de39c492e60939e48a86255ce65e7873a86ac61
EllenZC <32197594+EllenZC@users.noreply.github.com>
2018-11-14 15:06:16 +0100

    Update motob.py

--- proj6/motob.py
+++ proj6/motob.py
@@ -1,33 +1,14 @@
-from motors import Motors
-from behavior import *
-
+from motors import *
+from Behavior import *
 
 class Motob:
     def __init__(self):
         self.motors = Motors()
-        self.value = None
 
-    def update(self, recommended_behavior):
-        self.operationalize(recommended_behavior.motor_recommendations)
+    def update(self, motor_recommendation):
+        self.operationalize(motor_recommendation[0], motor_recommendation[1])
 
-    # settings should be a list consisting of max 3 elements:
-    # action (string/character)
-    # speed (float [-1, 1])
-    # duration (time in seconds)
-    def operationalize(self, settings):
-        if settings[0] == 'L':
-            self.motors.left(settings[1], settings[2])
-        elif settings[0] == 'R':
-            self.motors.right(settings[1], settings[2])
-        elif settings[0] == 'D':
-            self.dance()
-
-    def dance(self):
-        self.motors = Motors()
-        self.motors.forward(.2, 3)
-        self.motors.backward(.2, 3)
-        self.motors.right(.5, 3)
-        self.motors.left(.5, 3)
-        self.motors.backward(.3, 2.5)
-        self.motors.set_value([.5, .1], 10)
-        self.motors.set_value([-.5, -.1], 10)
+    # sets the right and left motors to left_speed and right_speed
+    def operationalize(self, left_speed, right_speed):
+        self.motors.set_left_speed(left_speed)
+        self.motors.set_right_speed(right_speed)
