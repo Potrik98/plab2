61f1fc02907a92b2c8a7ccac08e1fbc8e49045c7
Eivind Yu Nilsen <31821339+EivindYN@users.noreply.github.com>
2018-11-14 14:59:23 +0100

    Fix

--- proj6/StandardBehavior.py
+++ proj6/StandardBehavior.py
@@ -1,9 +1,7 @@
 from Behavior import Behavior
-from imager2 import *
-from camera import *
 
 
-class CameraBehavior(Behavior):
+class StandardBehavior(Behavior):
     def __init__(self, bbcon):
         Behavior.__init__(self, bbcon)
         self.priority = 1
@@ -11,5 +9,5 @@ class CameraBehavior(Behavior):
     def sense_and_act(self):
         self.halt_request = False
         self.match_degree = 1
-        self.motor_recommendation = (-1, -1)
+        self.motor_recommendation = [1, 1]
         self.weight = 1
